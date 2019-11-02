df14aafc5e17870b69899db871cdbafb2c0c2806
Oluwasanmi Aderibigbe <sanmiaderibigbe@outlook.com>
2019-11-02 05:28:13 +0100

    fixed chapter 3

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -43,7 +43,7 @@ public class InfoActivity extends AppCompatActivity {
     // ***
 
     public void createPhoneIntent(View view){
-        Intent phoneIntent =new Intent(Intent.ACTION_VIEW);
+        Intent phoneIntent =new Intent(Intent.ACTION_DIAL);
         phoneIntent.setData(Uri.parse("tel:0123456789"));
         startActivity(phoneIntent);
     }
